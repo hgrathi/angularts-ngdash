39acd0da2854cb798af22b057a1ba0e3e5f4ac4e
Harish Rathi <hgrathi@gmail.com>
2015-06-03 09:55:47 +0000

    added more checks

--- src/ngdash.js
+++ src/ngdash.js
@@ -45,13 +45,16 @@
             if(dash.isNullOrUndefined(value)) {return true;}
             if(dash.isFunction(value)) {return false;}
             if(dash.isString(value) && value === '') {return true;}
+            if(dash.isBoolean(value)) {return false;}
+            if(dash.isDate(value)) {return false;}
             if(dash.isNumber(value)) {return dash.isNaN(value);}
             if(dash.isArray(value)) {return value.length === 0;}
             if(dash.isPlainObject(value)) {
                 for(var prop in value) { if(value.hasOwnProperty(prop)) {return false; }}
                 return true;
             }
-            return false;
+            if(dash.isObject(value)) {return false;}
+            return dash.isEmpty(value);
         };
 
 })(window.angular);
