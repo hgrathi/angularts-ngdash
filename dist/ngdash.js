e44a3d00d0f5c84e5446d2e91a8d2155fb763c10
Harish Rathi <hgrathi@gmail.com>
2015-06-04 12:48:05 +0000

    [ patch ] Bumped version number : 2.1.7

--- dist/ngdash.js
+++ dist/ngdash.js
@@ -1,5 +1,4 @@
-
-(function (angular) {
+(function(angular) {
 
     'use strict';
 
@@ -9,15 +8,15 @@
 
     //////////////////////////////////////////////////////////////////
 
-    function ngdashProvider(){
+    function ngdashProvider() {
 
         var dash = window._;
 
         var provider = {
-            deleteGlobalReference : function(){
+            deleteGlobalReference: function() {
                 delete window._;
             },
-            $get : function (){
+            $get: function() {
                 return ngdashService(dash);
             }
         };
@@ -28,32 +27,60 @@
     //////////////////////////////////////////////////////////////////
 
     function ngdashService(dash) {
-        dash.isNullOrUndefined = function (value) { return isNullOrUndefined(dash, value); };
-        dash.isNullOrEmpty = function (value) { return isNullOrEmpty(dash, value); };
+        dash.isNullOrUndefined = function(value) {
+            return isNullOrUndefined(dash, value);
+        };
+        dash.isNullOrEmpty = function(value) {
+            return isNullOrEmpty(dash, value);
+        };
         return dash;
     }
 
     //////////////////////////////////////////////////////////////////
 
     function isNullOrUndefined(dash, value) {
-        if(dash.isUndefined(value)) { return true; }
-        if(dash.isNull(value)) {return true;}
+        if (dash.isUndefined(value)) {
+            return true;
+        }
+        if (dash.isNull(value)) {
+            return true;
+        }
         return false;
     }
 
     function isNullOrEmpty(dash, value) {
-        if(dash.isNullOrUndefined(value)) {return true;}
-        if(dash.isFunction(value)) {return false;}
-        if(dash.isString(value) && value === '') {return true;}
-        if(dash.isBoolean(value)) {return false;}
-        if(dash.isDate(value)) {return false;}
-        if(dash.isNumber(value)) {return dash.isNaN(value);}
-        if(dash.isArray(value)) {return value.length === 0;}
-        if(dash.isPlainObject(value)) {
-            for(var prop in value) { if(value.hasOwnProperty(prop)) {return false; }}
+        if (dash.isNullOrUndefined(value)) {
             return true;
         }
-        if(dash.isObject(value)) {return false;}
+        if (dash.isFunction(value)) {
+            return false;
+        }
+        if (dash.isString(value) && value === '') {
+            return true;
+        }
+        if (dash.isBoolean(value)) {
+            return false;
+        }
+        if (dash.isDate(value)) {
+            return false;
+        }
+        if (dash.isNumber(value)) {
+            return dash.isNaN(value);
+        }
+        if (dash.isArray(value)) {
+            return value.length === 0;
+        }
+        if (dash.isPlainObject(value)) {
+            for (var prop in value) {
+                if (value.hasOwnProperty(prop)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (dash.isObject(value)) {
+            return false;
+        }
         return dash.isEmpty(value);
     };
 
